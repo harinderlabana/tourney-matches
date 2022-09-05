ab23049b97ea877b10a1023babea2cf04325167f
Harinder Labana <sunny.labana@gmail.com>
2022-09-05 19:24:31 -0400

    created Match component

---
+++ src/components/Match.js
@@ -0,0 +1,13 @@
+function Match() {
+  return (
+    <article className="Match">
+      <h1>Joe <span>vs</span> Bob</h1>
+      {/* To be shown when there is a winner */}
+      <h2>Hoe is hte winner by 1!</h2>
+      {/* To be shown when there is no winner */}
+      <h2>No winners yet!</h2>
+    </article>
+  );
+}
+
+export default Match;
